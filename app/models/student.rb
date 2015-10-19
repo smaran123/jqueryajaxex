6940f33d7e8542ad7e7ecbef33e80c2cb55e48ce
rails bhagath <rails@gmail1.com>
2015-10-19 12:18:51 +0530

    validation error display using ajax

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,7 @@
 class Student < ActiveRecord::Base
+   validates :first_name,:last_name,:email, presence: true
+      validates :first_name,:last_name,       length: { minimum: 5 }
+      validates :phone, :presence => true, format: { with: /\A\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})\Z/,
+    message: "Number must be in xxx-xxx-xxxx format" }
+
 end
