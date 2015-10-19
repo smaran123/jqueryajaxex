fdfb6d6ee0b8180c1b46b09705df50c531632f90
rails bhagath <rails@gmail1.com>
2015-10-19 18:49:25 +0530

    role implemented

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,9 @@ before_action :configure_permitted_parameters, if: :devise_controller?
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :name
+   # devise_parameter_sanitizer.for(:sign_up) << :name, :role
+      devise_parameter_sanitizer.for(:sign_up) {|u| u.permit!}
+
   end
 
 end
